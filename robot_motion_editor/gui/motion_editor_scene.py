0c1e54d20cfbc0041e193dcd3ba55c63fa367cf5
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-04-24 18:52:38 +0900

    Fit arrow edge

--- robot_motion_editor/gui/motion_editor_scene.py
+++ robot_motion_editor/gui/motion_editor_scene.py
@@ -34,7 +34,7 @@ class FrameBlockItem(QGraphicsRectItem):
         self.selected_pen.setWidth(4)
         self.setPen(self.default_pen)
 
-        self.setFlags(self.ItemIsMovable | self.ItemIsSelectable)
+        self.setFlags(self.ItemIsMovable | self.ItemIsSelectable | self.ItemSendsGeometryChanges)
         self.name = frame_name
         self.uid = uid if uid is not None else frame_name
 
@@ -60,6 +60,23 @@ class FrameBlockItem(QGraphicsRectItem):
             self.setPen(self.default_pen)
         super().paint(painter, option, widget)
 
+    def itemChange(self, change, value):
+        if change == self.ItemPositionChange and self.scene() is not None:
+            for item in self.scene().items():
+                if isinstance(item, ArrowItem):
+                    if item.start_item == self or item.end_item == self:
+                        item.update_path(force_edge_snap=True)
+        return super().itemChange(change, value)
+
+
+def compute_edge_point(center, target, width, height):
+    dx = target.x() - center.x()
+    dy = target.y() - center.y()
+    if dx == 0 and dy == 0:
+        return center
+    scale = 0.5 / max(abs(dx) / width, abs(dy) / height)
+    return QPointF(center.x() + dx * scale, center.y() + dy * scale)
+
 
 class ArrowEndpointHandle(QGraphicsEllipseItem):
     def __init__(self, arrow, is_start):
@@ -79,7 +96,7 @@ class ArrowEndpointHandle(QGraphicsEllipseItem):
         for item in scene.items():
             if isinstance(item, FrameBlockItem):
                 item.set_highlighted(item.sceneBoundingRect().contains(pos))
-        self.arrow.update_path()
+        self.arrow.update_path(force_edge_snap=False)
 
     def mouseReleaseEvent(self, event):
         scene = self.scene()
@@ -88,25 +105,15 @@ class ArrowEndpointHandle(QGraphicsEllipseItem):
             if isinstance(item, FrameBlockItem):
                 target = item
                 break
-        if target:
-            if self.is_start:
-                self.arrow.start_item = target
-            else:
-                self.arrow.end_item = target
+        if self.is_start:
+            self.arrow.start_item = target
+        else:
+            self.arrow.end_item = target
         for item in scene.items():
             if hasattr(item, "set_highlighted"):
                 item.set_highlighted(False)
-        self.arrow.update_path()
+        self.arrow.update_path(force_edge_snap=True)
         super().mouseReleaseEvent(event)
-
-
-def compute_edge_point(center, target, width, height):
-    dx = target.x() - center.x()
-    dy = target.y() - center.y()
-    if dx == 0 and dy == 0:
-        return center
-    scale = 0.5 / max(abs(dx) / width, abs(dy) / height)
-    return QPointF(center.x() + dx * scale, center.y() + dy * scale)
 
 
 class ArrowItem(QGraphicsPathItem):
@@ -127,14 +134,21 @@ class ArrowItem(QGraphicsPathItem):
         scene.addItem(self.start_handle)
         scene.addItem(self.end_handle)
 
-    def update_path(self):
+    def update_path(self, force_edge_snap=False):
         path = QPainterPath()
+
         if self.start_item:
             start_center = self.start_item.sceneBoundingRect().center()
             end_center = self.end_item.sceneBoundingRect().center() if self.end_item else self.end_handle.scenePos()
             w = self.start_item.rect().width()
             h = self.start_item.rect().height()
-            p1 = compute_edge_point(start_center, end_center, w, h)
+            p1 = compute_edge_point(
+                start_center,
+                end_center,
+                w,
+                h) if force_edge_snap else self.start_handle.scenePos()
+            if force_edge_snap:
+                self.start_handle.setPos(p1)
         else:
             p1 = self.start_handle.scenePos()
 
@@ -143,7 +157,9 @@ class ArrowItem(QGraphicsPathItem):
             start_center = self.start_item.sceneBoundingRect().center() if self.start_item else self.start_handle.scenePos()
             w = self.end_item.rect().width()
             h = self.end_item.rect().height()
-            p2 = compute_edge_point(end_center, start_center, w, h)
+            p2 = compute_edge_point(end_center, start_center, w, h) if force_edge_snap else self.end_handle.scenePos()
+            if force_edge_snap:
+                self.end_handle.setPos(p2)
         else:
             p2 = self.end_handle.scenePos()
 
@@ -218,35 +234,23 @@ class MotionFlowScene(QGraphicsScene):
         used_uids = set()
         items = [item for item in self.items() if isinstance(item, FrameBlockItem)]
         items.sort(key=lambda i: i.uid)
-
         for idx, item in enumerate(items):
             if not hasattr(item, 'uid') or not item.uid:
                 item.uid = self._generate_uid(item.name)
             while item.uid in used_uids:
                 item.uid = self._generate_uid(item.name)
             used_uids.add(item.uid)
-
             uid = item.uid
             layout["block"][uid] = {
-                "info": {
-                    "type": "frame",
-                    "filename": item.name,
-                    "id": idx
-                },
-                "place": {
-                    "x": int(item.pos().x()),
-                    "y": int(item.pos().y())
-                },
+                "info": {"type": "frame", "filename": item.name, "id": idx},
+                "place": {"x": int(item.pos().x()), "y": int(item.pos().y())},
                 "connection": item.connection if hasattr(item, "connection") else {"output": {}}
             }
-
         for arrow in self.arrows:
             if arrow.start_item and arrow.end_item:
                 layout["arrow"][arrow.arrow_id] = {
-                    "info": {
-                        "id": int(arrow.arrow_id.split("_")[-1])
-                    },
-                    "waypoints": []  # waypoints 未実装
+                    "info": {"id": int(arrow.arrow_id.split("_")[-1])},
+                    "waypoints": []
                 }
         return layout
 
