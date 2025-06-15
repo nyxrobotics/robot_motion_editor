c78a43a6ad7fd08ffdf5664b5923d151276194da
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2025-06-16 01:15:11 +0900

    Refactoring

--- robot_motion_editor/robot_motion_editor/logic/animation_item_data.py
+++ robot_motion_editor/robot_motion_editor/logic/animation_item_data.py
@@ -2,6 +2,7 @@ import os
 
 from .frame_file_manager import FrameData
 from .if_condition_file_manager import IfConditionData
+
 from .switch_condition_file_manager import SwitchConditionData
 
 
@@ -17,13 +18,16 @@ class AnimationItemData:
     def get_frame(self, filename):
         if filename in self.frames:
             return self.frames[filename]
-        return self.load_frame(filename)
+        return self._load_frame(filename)
 
     def set_frame(self, filename, frame_data):
+        old_data = self.frames.get(filename)
+        if old_data and frame_data.get_dict() == old_data.get_dict():
+            return
         self.frames[filename] = frame_data
-        self.save_frame(filename)
+        self._save_frame(filename)
 
-    def load_frame(self, filename):
+    def _load_frame(self, filename):
         frame = FrameData(joint_names=self.motion_directory_manager.get_joint_names())
         path = self.motion_directory_manager.resolve_frame_path(filename)
         if os.path.exists(path):
@@ -31,7 +35,7 @@ class AnimationItemData:
         self.frames[filename] = frame
         return frame
 
-    def save_frame(self, filename):
+    def _save_frame(self, filename):
         if filename in self.frames:
             path = self.motion_directory_manager.resolve_frame_path(filename)
             self.frames[filename].save_to_file(path)
@@ -40,13 +44,15 @@ class AnimationItemData:
     def get_initial_frame(self):
         if self.initial_frame:
             return self.initial_frame
-        return self.load_initial_frame()
+        return self._load_initial_frame()
 
     def set_initial_frame(self, frame_data):
+        if self.initial_frame and frame_data.get_dict() == self.initial_frame.get_dict():
+            return
         self.initial_frame = frame_data
-        self.save_initial_frame()
+        self._save_initial_frame()
 
-    def load_initial_frame(self):
+    def _load_initial_frame(self):
         frame = FrameData(joint_names=self.motion_directory_manager.get_joint_names())
         path = self.motion_directory_manager.resolve_initial_frame_path()
         if os.path.exists(path):
@@ -54,7 +60,7 @@ class AnimationItemData:
         self.initial_frame = frame
         return frame
 
-    def save_initial_frame(self):
+    def _save_initial_frame(self):
         if self.initial_frame:
             path = self.motion_directory_manager.resolve_initial_frame_path()
             self.initial_frame.save_to_file(path)
@@ -63,13 +69,16 @@ class AnimationItemData:
     def get_if(self, filename):
         if filename in self.ifs:
             return self.ifs[filename]
-        return self.load_if(filename)
+        return self._load_if(filename)
 
     def set_if(self, filename, if_data):
+        old_data = self.ifs.get(filename)
+        if old_data and if_data.get_dict() == old_data.get_dict():
+            return
         self.ifs[filename] = if_data
-        self.save_if(filename)
+        self._save_if(filename)
 
-    def load_if(self, filename):
+    def _load_if(self, filename):
         cond = IfConditionData()
         path = self.motion_directory_manager.resolve_if_condition_path(filename)
         if os.path.exists(path):
@@ -77,7 +86,7 @@ class AnimationItemData:
         self.ifs[filename] = cond
         return cond
 
-    def save_if(self, filename):
+    def _save_if(self, filename):
         if filename in self.ifs:
             path = self.motion_directory_manager.resolve_if_condition_path(filename)
             self.ifs[filename].save_to_file(path)
@@ -86,13 +95,16 @@ class AnimationItemData:
     def get_switch(self, filename):
         if filename in self.switches:
             return self.switches[filename]
-        return self.load_switch(filename)
+        return self._load_switch(filename)
 
     def set_switch(self, filename, switch_data):
+        old_data = self.switches.get(filename)
+        if old_data and switch_data.get_dict() == old_data.get_dict():
+            return
         self.switches[filename] = switch_data
-        self.save_switch(filename)
+        self._save_switch(filename)
 
-    def load_switch(self, filename):
+    def _load_switch(self, filename):
         cond = SwitchConditionData()
         path = self.motion_directory_manager.resolve_switch_condition_path(filename)
         if os.path.exists(path):
@@ -100,7 +112,7 @@ class AnimationItemData:
         self.switches[filename] = cond
         return cond
 
-    def save_switch(self, filename):
+    def _save_switch(self, filename):
         if filename in self.switches:
             path = self.motion_directory_manager.resolve_switch_condition_path(filename)
             self.switches[filename].save_to_file(path)
@@ -111,19 +123,19 @@ class AnimationItemData:
         Load all items (initial frame, frames, if conditions, switch conditions)
         into memory to minimize file access during operation.
         """
-        self.load_initial_frame()
+        self._load_initial_frame()
 
         frame_files = self.motion_directory_manager.list_frame_files()
         for frame_name in frame_files:
-            self.load_frame(frame_name)
+            self._load_frame(frame_name)
 
         if_files = self.motion_directory_manager.list_if_condition_files()
         for if_name in if_files:
-            self.load_if(if_name)
+            self._load_if(if_name)
 
         switch_files = self.motion_directory_manager.list_switch_condition_files()
         for switch_name in switch_files:
-            self.load_switch(switch_name)
+            self._load_switch(switch_name)
 
     # ======== Clear Cache ========
     def clear(self):
